cd436ed121dd6bf33c9f24a30f16a4ae5b76d417
jeyroik <jeyroik@gmail.com>
2020-06-04 16:45:07 +0300

    up to installer v3

--- tests/ExtensionTest.php
+++ tests/ExtensionTest.php
@@ -1,11 +1,12 @@
 <?php
 namespace tests;
 
+use Dotenv\Dotenv;
 use extas\components\extensions\Extension;
 use extas\components\extensions\ExtensionRepository;
 use extas\components\extensions\ExtensionRepositoryGet;
 use extas\components\Item;
-use extas\components\SystemContainer;
+use extas\components\repositories\TSnuffRepository;
 use extas\interfaces\extensions\IExtensionRepositoryGet;
 use extas\interfaces\repositories\IRepository;
 use PHPUnit\Framework\TestCase;
@@ -18,30 +19,36 @@ use PHPUnit\Framework\TestCase;
  */
 class ExtensionTest extends TestCase
 {
+    use TSnuffRepository;
+
     protected IRepository $extRepo;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
+        $env = Dotenv::create(getcwd() . '/tests/');
         $env->load();
         $this->extRepo = new ExtensionRepository();
+        $this->registerSnuffRepos([
+            'extensionRepository' => ExtensionRepository::class,
+            'testRepository' => SomeRepository::class
+        ]);
     }
 
     public function tearDown(): void
     {
-        $this->extRepo->delete([Extension::FIELD__CLASS => ExtensionRepositoryGet::class]);
+        $this->unregisterSnuffRepos();
     }
 
     public function testGetRepository()
     {
-        $this->extRepo->create(new Extension([
+        $this->createWithSnuffRepo('extensionRepository', new Extension([
             Extension::FIELD__CLASS => ExtensionRepositoryGet::class,
             Extension::FIELD__INTERFACE => IExtensionRepositoryGet::class,
             Extension::FIELD__SUBJECT => '*',
             Extension::FIELD__METHODS => ['testRepository']
         ]));
-        SystemContainer::addItem('testRepository', SomeRepository::class);
+
         $extendable = new class extends Item {
             protected function getSubjectForExtension(): string
             {
